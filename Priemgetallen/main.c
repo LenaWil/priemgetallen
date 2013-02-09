d31c22045262606441bb1f4b1d29a7d04ad25844
Tijmen Wildervanck <tijmenwildervanck@MacBook-van-Tijmen-Wildervanck.local>
2013-02-09 08:24:23 +0100

    Initial Commit

---
+++ Priemgetallen/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Priemgetallen
+//
+//  Created by Tijmen Wildervanck on 09-02-13.
+//  Copyright (c) 2013 CGU. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
